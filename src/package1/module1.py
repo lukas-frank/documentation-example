86a9f489b5d6efd00726b4b532dfa75ab9a2870c
Lukas Frank <79790052+lukas-frank@users.noreply.github.com>
2025-10-15 12:13:44 +0200

    Create module1.py
    Create a test class and test function for documentation

---
+++ src/package1/module1.py
@@ -0,0 +1,26 @@
+
+class TestClass:
+    """
+    This Class does nothing.
+    """
+    pass
+
+    def do_nothing(self, dummy: int) -> float:
+        """
+        This method does nothing.
+
+        :param dummy: int, ignored variable
+        :return: 0.0 (always)
+        """
+        return 0.0
+
+def dummy_fn(param: int):
+    """
+    This function does nothing
+
+    :param dummy: int, ignored.
+    :return: None.
+    """
+    pass
+  
+
